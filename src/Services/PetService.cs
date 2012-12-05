181e2a6e9e16d31fac51e26a6125752a4fecd721
Afonso Henrique Ferreira Cunha <afonso.sancho@gmail.com>
2012-12-05 18:29:19 -0200

    Crud Pet Service Two Version of Dog, Cat and Parrot Service

--- src/Services/PetService.cs
+++ src/Services/PetService.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Tony Alexander Hild <tony_hild@yahoo.com>
 //
-// Copyright (c) 2012 
+// Copyright (p) 2012 
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -31,40 +31,51 @@ using System.Collections.Generic;
 using System.Linq;
 using ServiceStack.ServiceInterface;
 
-namespace GestUAB
+namespace HelloServices
 {
-    /// <summary>
-    /// Define your ServiceStack web service request (i.e. the Request DTO).
-    /// </summary>
-    [Route("/pets")]
-    [Route("/pets/{Id}")]
-    public class Pet
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; }
-    }
-    
-   
     /// <summary>
     /// Create your ServiceStack web service implementation.
     /// </summary>
     public class PetService : RestServiceBase<Pet>
     {
-        public override object OnGet (Pet pet)
+        public override object OnGet(Pet pet)
         {
-            if (pet.Id == Guid.Empty) {
-                return from n in PetDatabase.Instace.Pets 
-                    select n;
+            if (pet.Id == Guid.Empty)
+            {
+                return from n in PetDatabase.Instace.Pets
+                       select n;
             }
-            return (from n in PetDatabase.Instace.Pets 
-                   where n.Id == pet.Id
-                   select n).SingleOrDefault();
+            return (from n in PetDatabase.Instace.Pets
+                    where n.Id == pet.Id
+                    select n).SingleOrDefault();
+        }
+
+        public override object OnDelete(Pet pet)
+        {
+            if (pet.Id == Guid.Empty)
+            {
+                foreach (Pet p in PetDatabase.Instace.Pets)
+                {
+                    PetDatabase.Instace.Pets.Remove(p);
+                }
+            }
+            else
+            {
+                foreach (Pet p in PetDatabase.Instace.Pets)
+                {
+                    if (p.Id == pet.Id)
+                    {
+                        PetDatabase.Instace.Pets.Remove(p);
+                    }
+                }
+            }
+
+            return base.OnDelete(pet);
         }
     }
 
-    [Route("/dogs")]
-    [Route("/dogs/{Id}")]
+    [Route("/v1.0/dogs")]
+    [Route("/v1.0/dogs/{Id}")]
     public class Dog : Pet
     {
     }
@@ -74,21 +85,68 @@ namespace GestUAB
     /// </summary>
     public class DogService : RestServiceBase<Dog>
     {
-        public override object OnGet (Dog dog)
+        public override object OnGet(Dog dog)
         {
-         if (dog.Id == Guid.Empty) {
-                return from n in PetDatabase.Instace.Pets 
-                    where n.GetType () == typeof(Dog)
-                    select n;
+            if (dog.Id == Guid.Empty)
+            {
+                return from n in PetDatabase.Instace.Pets
+                       where n.GetType() == typeof(Dog)
+                       select n;
             }
-            return (from n in PetDatabase.Instace.Pets 
-                where n.Id == dog.Id
-                select  n).SingleOrDefault ();
+            return (from n in PetDatabase.Instace.Pets
+                    where n.Id == dog.Id
+                    select n).SingleOrDefault();
         }
     }
 
-    [Route("/cats")]
-    [Route("/cats/{Id}")]
+    [Route("/v2.0/dogs")]
+    [Route("/v2.0/dogs/{Id}")]
+    public class DogNewService : Dog
+    {
+    }
+
+    /// <summary>
+    /// Create your ServiceStack web service implementation.
+    /// </summary>
+    public class DogServiceNew : RestServiceBase<DogNewService>
+    {
+        public override object OnGet(DogNewService dog)
+        {
+            if (dog.Id == Guid.Empty)
+            {
+                return from n in PetDatabase.Instace.Pets
+                       where n.GetType() == typeof(Dog)
+                       select n;
+            }
+            return (from n in PetDatabase.Instace.Pets
+                    where n.Id == dog.Id
+                    select n).SingleOrDefault();
+        }
+
+        public override object OnPut(DogNewService dog)
+        {
+            DogNewService q = (from n in PetDatabase.Instace.Pets where n.Id == dog.Id select n).FirstOrDefault() as DogNewService;
+
+            q.Name = dog.Name;
+            return q;
+        }
+
+        public override object OnPost(DogNewService dog)
+        {
+            PetDatabase.Instace.Pets.Add(dog);
+            return dog;
+        }
+
+        public override object OnDelete(DogNewService dog)
+        {
+            PetDatabase.Instace.Pets.Remove(dog);
+
+            return dog;
+        }
+    }
+
+    [Route("/v2.0/cats")]
+    [Route("/v2.0/cats/{Id}")]
     public class Cat : Pet
     {
     }
@@ -98,21 +156,42 @@ namespace GestUAB
     /// </summary>
     public class CatService : RestServiceBase<Cat>
     {
-        public override object OnGet (Cat cat)
+        public override object OnGet(Cat cat)
         {
-         if (cat.Id == Guid.Empty) {
-                return from n in PetDatabase.Instace.Pets 
-                    where n.GetType () == typeof(Cat)
-                    select n;
+            if (cat.Id == Guid.Empty)
+            {
+                return from n in PetDatabase.Instace.Pets
+                       where n.GetType() == typeof(Cat)
+                       select n;
             }
-            return (from n in PetDatabase.Instace.Pets 
-                where n.Id == cat.Id
-                select  n).SingleOrDefault ();
+            return (from n in PetDatabase.Instace.Pets
+                    where n.Id == cat.Id
+                    select n).SingleOrDefault();
+        }
+        public override object OnPut(Cat cat)
+        {
+            Cat q = (from n in PetDatabase.Instace.Pets where n.Id == cat.Id select n).FirstOrDefault() as Cat;
+
+            q.Name = cat.Name;
+            return q;
+        }
+
+        public override object OnPost(Cat cat)
+        {
+            PetDatabase.Instace.Pets.Add(cat);
+            return cat;
+        }
+
+        public override object OnDelete(Cat cat)
+        {
+            PetDatabase.Instace.Pets.Remove(cat);
+
+            return cat;
         }
     }
 
-    [Route("/parrots")]
-    [Route("/parrots/{Id}")]
+    [Route("/v2.0/parrots")]
+    [Route("/v2.0/parrots/{Id}")]
     public class Parrot : Pet
     {
     }
@@ -122,22 +201,44 @@ namespace GestUAB
     /// </summary>
     public class ParrotService : RestServiceBase<Parrot>
     {
-        public override object OnGet (Parrot parrot)
+        public override object OnGet(Parrot parrot)
         {
-         if (parrot.Id == Guid.Empty) {
-                return from n in PetDatabase.Instace.Pets 
-                    where n.GetType () == typeof(Parrot)
-                    select n;
+            if (parrot.Id == Guid.Empty)
+            {
+                return from n in PetDatabase.Instace.Pets
+                       where n.GetType() == typeof(Parrot)
+                       select n;
             }
-            return (from n in PetDatabase.Instace.Pets 
-                where n.Id == parrot.Id
-                select  n).SingleOrDefault ();
+            return (from n in PetDatabase.Instace.Pets
+                    where n.Id == parrot.Id
+                    select n).SingleOrDefault();
+        }
+
+        public override object OnPut(Parrot parrot)
+        {
+            Parrot q = (from n in PetDatabase.Instace.Pets where n.Id == parrot.Id select n).FirstOrDefault() as Parrot;
+
+            q.Name = parrot.Name;
+            return q;
+        }
+
+        public override object OnPost(Parrot parrot)
+        {
+            PetDatabase.Instace.Pets.Add(parrot);
+            return parrot;
+        }
+
+        public override object OnDelete(Parrot parrot)
+        {
+            PetDatabase.Instace.Pets.Remove(parrot);
+
+            return parrot;
         }
     }
-   
+
 
     /// <summary>
-    /// Define your ServiceStack web service request (i.e. the Request DTO).
+    /// Define your ServiceStack web service parrot (i.e. the Request DTO).
     /// </summary>
     [Route("/pets/type/{PetType}")]
     public class Pets
@@ -150,15 +251,15 @@ namespace GestUAB
     /// </summary>
     public class PetsService : RestServiceBase<Pets>
     {
-        public override object OnGet (Pets pets)
+        public override object OnGet(Pets pets)
         {
-//            if (string.IsNullOrEmpty (pets.PetType)) {
-//                return from n in PetDatabase.Instace.Pets 
-//                    select n;
-//            }
-            return from n in PetDatabase.Instace.Pets 
-                where n.GetType ().Name.ToLower() == pets.PetType.ToLower()
-                select n;
+            //            if (string.IsNullOrEmpty (pets.PetType)) {
+            //                return from n in PetDatabase.Instace.Pets 
+            //                    select n;
+            //            }
+            return from n in PetDatabase.Instace.Pets
+                   where n.GetType().Name.ToLower() == pets.PetType.ToLower()
+                   select n;
         }
     }
 
@@ -166,30 +267,36 @@ namespace GestUAB
     {
 
         static PetDatabase _instance;
-        static object _lock = new object ();
-        IList <Pet> _pets = new List<Pet> ();
+        static object _lock = new object();
+        IList<Pet> _pets = new List<Pet>();
 
-        public Pet this [int index] {
-            get{ return _pets [index];}
+        public Pet this[int index]
+        {
+            get { return _pets[index]; }
         }
 
-        public IList<Pet> Pets {
-            get { return _pets;}
+        public IList<Pet> Pets
+        {
+            get { return _pets; }
         }
 
-        public static PetDatabase Instace {
-            get {
-                if (_instance == null) {
-                    lock (_lock) {
-                        _instance = new PetDatabase ();
-                        _instance._pets.Add (new Dog (){Name = "Spike", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Dog (){Name = "Bo", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Dog (){Name = "Mike", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Cat (){Name = "Kitty", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Cat (){Name = "Mimi", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Cat (){Name = "FurBall", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Parrot (){Name = "Polly", Id = Guid.NewGuid()});
-                        _instance._pets.Add (new Parrot (){Name = "Rico", Id = Guid.NewGuid()});
+        public static PetDatabase Instace
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance = new PetDatabase();
+                        _instance._pets.Add(new Dog() { Name = "Spike", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Dog() { Name = "Bo", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Dog() { Name = "Mike", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Cat() { Name = "Kitty", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Cat() { Name = "Mimi", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Cat() { Name = "FurBall", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Parrot() { Name = "Polly", Id = Guid.NewGuid() });
+                        _instance._pets.Add(new Parrot() { Name = "Rico", Id = Guid.NewGuid() });
                     }
                 }
                 return _instance;
